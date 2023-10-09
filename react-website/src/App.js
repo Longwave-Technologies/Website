b79e7ac1818f7842f13e4bbdb862ba1f3b78690b
scripting saga <scriptingsaga@gmail.com>
2023-10-09 15:57:46 -0500

    feature:loadingAnimationTransition completed after Header branch

--- react-website/src/App.js
+++ react-website/src/App.js
@@ -1,14 +1,27 @@
 import './App.css';
-import React from 'react';
+import React, { useState, useEffect }  from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import AppRouter from './routes/AppRouter';
 import LoadingScreen from './pages/loadingScreen/LoadingScreen';
 
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    console.log('isLoading:', isLoading);
+    // Simulate data loading or any other initialization
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 4100); // Simulate a 2-second loading time
+  }, []);
+
   return (
-    <div>
-      <AppRouter />
+    <div className="App">
+      {isLoading ? <LoadingScreen /> : (
+       console.log('AppRouter displayed'),
+        <AppRouter />
+      )}
     </div>
   );
 }
--- react-website/src/pages/loadingScreen/loadingScreen.js
+++ react-website/src/pages/loadingScreen/loadingScreen.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './LoadingScreen.css'; // Create this CSS file for styling
+import { default as introSVG } from '../../assets/images/logoFinalAnimation.svg' ; 
 
-function LoadingScreen () {
+const LoadingScreen = () => {
+  const [animationComplete, setAnimationComplete] = useState(false);
+
+
+  // Simulate an animation delay
+  useEffect(() => {
+    console.log('animationComplete:', animationComplete);
+
+    const animationTimeout = setTimeout(() => {
+      setAnimationComplete(true);
+    }, 1000); // Change the time as needed
+    
+    console.log('animationComplete:', animationComplete);
+
+    return () => clearTimeout(animationTimeout);
+  }, [animationComplete]);
+
+
   return (
-    // <div>
-      <h2>LoadingScreen</h2>
-      // <p>Welcome to the Home page.</p>
-    // </div>
+    <div className={`loadingScreen${animationComplete ? ' animationComplete' : ''
+    }`}>
+        <img src={introSVG} alt="introSVG"  className="introSVG"/>
+    </div>
+
   );
 };
 
