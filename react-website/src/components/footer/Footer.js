cd8fbccfb95189aa3098499988129696bf0524c5
Benjamin Huang <benjamin.huang@alumni.stonybrook.edu>
2023-10-15 16:30:56 -0400

    updated footer to always show current year, and made font smaller.

--- react-website/src/components/footer/Footer.js
+++ react-website/src/components/footer/Footer.js
@@ -1,23 +1,30 @@
-import React, { useState, useEffect }  from 'react';
-import './Footer.css';
-import logoFooter from '../../assets/images/logoFinal.png';
+import React, { useState, useEffect } from "react";
+import "./Footer.css";
+import logoFooter from "../../assets/images/logoFinal.png";
 
-function Footer () {
-    const scrollToTop = () => {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-      };
-
-      return (
-        <footer className="footer">
-          <div className="footer-content">
-            <div className="logoFooter" onClick={scrollToTop}>
-                <img src={logoFooter} className='logoFooter' onClick={scrollToTop} alt='logoFooter' size={10}>
-                </img>
-            </div>
-            <p>&copy; 2023 Longwave Technologies L.I. Inc. All Rights Reserved.</p>
-            
-          </div>
-        </footer>
-      );
-    };
+function Footer() {
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+  const today = new Date();
+  const year = today.getFullYear();
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="logoFooter" onClick={scrollToTop}>
+          <img
+            src={logoFooter}
+            className="logoFooter"
+            onClick={scrollToTop}
+            alt="logoFooter"
+            size={10}
+          ></img>
+        </div>
+        <p>
+          &copy; {year} Longwave Technologies L.I. Inc. All Rights Reserved.
+        </p>
+      </div>
+    </footer>
+  );
+}
 export default Footer;
---
+++ react-website/src/pages/productPage/ProductPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ProductPage () {
+  return (
+    // <div>
+      <h2>ProductPage</h2>
+      // <p>Welcome to the Home page.</p>
+    // </div>
+  );
+};
+
+export default ProductPage;
