8567be1550c1a795848773ad52a6d2ae23dc2da3
Benjamin Huang <benjamin.huang@alumni.stonybrook.edu>
2023-10-17 14:38:01 -0400

    update

--- react-website/src/routes/AppRouter.js
+++ react-website/src/routes/AppRouter.js
@@ -1,13 +1,7 @@
 import React from "react";
 import "./AppRouter.css";
 
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Link,
-  useNavigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "../pages/homePage/HomePage";
 import Header from "../components/header/Header";
 import Services from "../pages/servicePage/ServicePage";
@@ -16,7 +10,7 @@ import Contact from "../pages/contactPage/ContactPage";
 import Footer from "../components/footer/Footer";
 
 const tabsData = [
-   { label: "Services", path: "/services" },
+  { label: "Services", path: "/services" },
   { label: "Products", path: "/products" },
   { label: "Contact", path: "/contact" },
 ];
@@ -29,13 +23,13 @@ function AppRouter() {
         <div class="dot2"></div>
         <div class="dot1"></div>
       </div>
-    
+
       <Router basename="/">
         <Header tabsData={tabsData} />
 
         <Routes>
           <Route path="/" exact element={<Home />} />
-          <Route path="/services"  element={<Services />} />
+          <Route path="/services" element={<Services />} />
           <Route path="/products" element={<Products />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
