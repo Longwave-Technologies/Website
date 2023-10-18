eaa66159a70145e3b276c93db02cc0ff782075e3
Benjamin Huang <benjamin.huang@alumni.stonybrook.edu>
2023-10-18 17:35:06 -0400

    format

--- react-website/src/pages/productPage/ProductPage.js
+++ react-website/src/pages/productPage/ProductPage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "../../styles/styles.css";
+import "./ProductPage.css";
+import Fade from "react-reveal/Fade";
 
 function ProductPage() {
   return (
     <div className="content">
-      <div>
+      <div className="product-container">
         <h2>Products page is a work in progress! Check in next week</h2>
       </div>
     </div>
--- react-website/src/pages/servicePage/ServicePage.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "../../styles/styles.css";
-import "./ServicePage.css";
-
-function ServicePage() {
-  return <div className="info-section-text-div"></div>;
-}
-
-export default ServicePage;
